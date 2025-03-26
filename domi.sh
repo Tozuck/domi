6421c5dfad53662b6b56ee5bc486331231d379e7
Tozuck <122993981+Tozuck@users.noreply.github.com>
2025-03-26 04:28:32 +0330

    Update domi.sh

--- domi.sh
+++ domi.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-set -e  # Exit on any error
-
-# Function to log and print messages
 echo_info() {
   echo -e "\033[1;32m[INFO]\033[0m $1"
 }
@@ -10,47 +7,29 @@ echo_error() {
   echo -e "\033[1;31m[ERROR]\033[0m $1"
   exit 1
 }
+curl -sSL https://raw.githubusercontent.com/Tozuck/Node_monitoring/main/node_monitor.sh | bash
+apt-get update; apt-get install curl socat git nload -y
 
-# Check for root privileges
-if [ "$EUID" -ne 0 ]; then
-  echo_error "This script must be run as root. Use sudo."
-fi
-
-# Update system packages
-echo_info "Updating package lists..."
-apt-get update
-
-# Install required packages
-echo_info "Installing required packages..."
-apt-get install -y curl socat git ufw || echo_error "Failed to install packages."
-
-
-# Install Docker
-echo_info "Installing Docker..."
 if ! command -v docker &> /dev/null; then
   curl -fsSL https://get.docker.com | sh || echo_error "Docker installation failed."
 else
   echo_info "Docker is already installed."
 fi
 
-# Clone Marzban Node repository
-echo_info "Cloning Marzban Node repository..."
-if [ ! -d "~/Marzban-node" ]; then
-  git clone https://github.com/Gozargah/Marzban-node ~/Marzban-node || echo_error "Failed to clone Marzban Node repository."
-else
-  echo_info "Marzban Node repository already exists."
-fi
+rm -r Marzban-node
 
-# Create necessary directories
-echo_info "Creating necessary directories..."
-mkdir -p /var/lib/marzban-node
+git clone https://github.com/Gozargah/Marzban-node
 
-# Generate Docker Compose configuration
-echo_info "Creating Docker Compose configuration..."
+rm -r /var/lib/marzban-node
+
+mkdir /var/lib/marzban-node
+
+rm ~/Marzban-node/docker-compose.yml
+
 cat <<EOL > ~/Marzban-node/docker-compose.yml
 services:
   marzban-node:
-    image: gozargah/marzban-node:latest
+    image: gozargah/marzban-node:v0.4.1
     restart: always
     network_mode: host
     environment:
@@ -62,44 +41,49 @@ services:
       - /var/lib/marzban-node:/var/lib/marzban-node
 EOL
 
-# Generate placeholder SSL certificate
-echo_info "Creating placeholder SSL certificate..."
+rm /var/lib/marzban-node/ssl_client_cert.pem
+
 cat <<EOL > /var/lib/marzban-node/ssl_client_cert.pem
 -----BEGIN CERTIFICATE-----
 MIIEnDCCAoQCAQAwDQYJKoZIhvcNAQENBQAwEzERMA8GA1UEAwwIR296YXJnYWgw
-IBcNMjQxMDI4MjA0MzQ5WhgPMjEyNDEwMDQyMDQzNDlaMBMxETAPBgNVBAMMCEdv
-emFyZ2FoMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAsnKzvHaUGoIN
-baono/8D6qiJ0Ref+b+ZFNJYWGtpxzu9wuLmIM9t5pa3P+iC52o5+nn3dFbslDjX
-a0tmr7rGiW/eNjScFrN6xBKG+H3gOHu9PXa02Jm74AbR1s2qm08bDYJuaerR/9Ww
-Q71318kmLD2GSk9WpOSkBN1GjN5MObUikL+uEK1q2EznCEdcFfbJOoU6UuZwrL2R
-xJdDn6U4uoH4r7+i028G9b5TDld5uj+1+DXlzktTU/HCPz6CIfQUS+ZKYW93xrwk
-9+ZraXIC386I36WXPr9V2UdwAXhXD/NtNPBkoozeZ+Dsb2mgQh11fySymaSjeJ/E
-5JzYyBG9kHNtuxwhaDnB77afClOaRUbBczA84T9RZMatTUyw2ofpw6lFHPa9Z3lt
-bzSuoV6vdyXbSDFAWsDPbQvmbr+WZFJhayJn4pNkz/IqK7ma2DnvRtb0koYg508d
-s6o/N6e0Gp98VN2objhrDt9PkMj/khZ2XvbPZ4uLPpzIeeeoyOT3UogQnloSdBkO
-RpINEgVJKUaKphcQZTjeUFJWOxaglAgOWGw22Mj7M2fh1PeyrYPAFFamaqxDsE6b
-WbLnIBsp0g95lbFiSJHQpOs9zakXlnW7sHa5RGSQKreqI/hVxxirlGFPZ9fc2mBe
-QKhK/R9Yv99BEdmaust2yg9qkqyxbwcCAwEAATANBgkqhkiG9w0BAQ0FAAOCAgEA
-dp05H45ZHG5ZEGjsM9b6bd4jxE7dsszs2tBZo0htO7Pp/RMQDfMp5RZpoX6Cer8Y
-k5+IvZgAsvCZ6AGeiTj4RaYQxxVx/+tgBc968kGlbsi3GmzyggQv7fNbRqrQN3EM
-BJEzWljb5FzWbzQglH7NhditgUzTTRsvY733n1o9wVbke2rL0Jn4MBrTVhSNojYB
-SQ8HFFvodKDO6KaAxyekUGesmeQcUbSWUrhm+obgmgqRntuEAbKWE+H4qHZzyvXQ
-uA15ijWy1uIJUpU/qhjb5JRz6XTPBZMnujRC54UTQFdlcZRXR56h5+oXU7pH+joJ
-oZ4e8qIGHDVN6knu8wxnNkPeu7D+Lu96Cj+DO08EAWyRz/W4D2aiAy5iG++3FYUV
-oJfm4YuBdmM4QEbjOAqNgDY7wm5lbrvbw9Z0XX0+wOnlg8UyJEPBRtDPIBnIdrJI
-aqAOni9dfafIweVQ8nExac34fJzs2CirQ8IxSkZiOtNEnmHdxMVW8JMhxd6Tsf4v
-RknZT7uw6P75RT7vic+v0ibyq7gJ3uKUhR71L2L2uAwRnNVfJMqWPiVc/jGrXEOu
-OgF5VRBvaEo3VD4UvPlALkuNzre5QYqLHE3G7p+g9FmNvzdGjiJ+3Ln8UlXhuE/I
-B+igjMPYdj7QLkFVVWQpWEq3RuUc8usdY4kzmJjOQA4=
+IBcNMjUwMzI2MDAzMDA0WhgPMjEyNTAzMDIwMDMwMDRaMBMxETAPBgNVBAMMCEdv
+emFyZ2FoMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA4cm9yAbu12g6
+uAL4TZz9eB6t58Bv0e3OWmbaheTzxCNO2d5XskwVTH5FaB0F8fQjC+vm6lCraTKK
+G1bbFcYt1X7U+Z6UimdILYxwkFkc4FmzrfeeMLR9zBxGuUQe26iFFvYKb6pU/mRr
+co7kohX5pX5iB0rTeOvywmZUZpbsBYBeTK1CMOb6Y24E42c0A80j+ipzVRDz/6qb
+Le86Hnl2qD3w38UshiLYtMRBV+TfKInyWFC6VzQ3+SEwdjMcZDpMTVNaPnt27lhD
+hfvbpmfwtzhu5NRKRFQInr8VsbIvBfYRZSW/+1PaTl0jqqUexPUB4R/YM+rWMZS7
+aZIo+9VD4O1DuDFi3pLa+nGdnlEDibSRPbOvgn32yAK7egG13ZA+7riS0z7DiChN
+AH4GSuUTTgkve9H70ArHBQ+/LTSDlycJ1uuArQ42Wm7mp1Md3Rm8b6ysH4XUUJhY
+dbdQx4kcb1xMZR47iy+O5Zf0LNkX3KO8rCGi5Y3K5mpgvpTpMjxPO3/uyJUrQdiB
+IBvAjJnyWkiNNeQYyd6hbs3ATxwxhdMaj2G6la+0vtSKWDO0Ej7ApYWNIZQQymmW
+dfQ2aABNB0/xRNCPn36zL+s86dIeLKBpfsNgQSl111vn+Eo3mRaLjtFLPu7ptOsG
+BwBv5wQyGiEr/3VY58qaC74moKoFk2ECAwEAATANBgkqhkiG9w0BAQ0FAAOCAgEA
+Lk2bH9hWlCh98UOhagoYD10w/MeLY9AXZhX4zO5UXTh8vGI9T0GdOrP3RwqWBifH
+YO01i1sq2+NYGK2IhSvM8oO+l5p0U9ZdOQuEWyZAPjBW7QDJ4HFWZEugR8LaLyVQ
+H5hhQ0WWMIJweP3V8rDkHjJVVtWCfecqGIXN+uVACGFd+WMz2mS5/Iq98+AWTllW
+iZItY4vYefMfzhgSda5KH9Ns+okXxRLT0+veqVC+XJJTfbrEyIqACEiowQA4xwOM
+hXYs7uzWqZ4QY55v7zd/6q2bu6CQ5M7yDmvw+6qlCDmuE0zLRNe0GmB23EoVEm9E
+Kvtl8K1gt0G+lmyQZRUTj0/lFMY7Mu9blK68U4d/pZjTfLn276JBHwChhqX2pNRI
+Y6uz1yA5Y4oof2yEtXXJgB71gaqVYYm0ed3XgM03e0cDB6BLU3iTbLlUite8qtHC
+OLN3yz9hZC6oGLPI5RZlDhJgsk9A6BhY21o4YejvpKf0rGhak3Y2tpQYRDy8XLiF
+3Pn9Ao9Dnn6nkXvUcm27CpJlY/UOA+rrhIgRc6/ofydR0rxk4JBwfs3vl4S3uufz
+62kNUQOcPdk80s/W7i9S/h0xOtxPLRyt+GDz+CUI454KYn+suBwBqzFpnID/+PFk
+Axte9/Qc7S2vSdkcgq3Xd+1TA69sErRlegSS80ukZwY=
 -----END CERTIFICATE-----
 EOL
 
-# Start the Marzban Node service
-echo_info "Starting Marzban Node service..."
 cd ~/Marzban-node
-docker compose up -d || echo_error "Failed to start Marzban Node service."
+docker compose up -d
 
+echo_info "Finalizing UFW setup..."
 
+ufw allow 22
+ufw allow 80
+ufw allow 2096
+ufw allow 2053
+ufw allow 62050
+ufw allow 62051
 
-# Sleep for 3 seconds before closing the connection
-echo_info "Setup complete. Marzban Node is now running..."
+ufw --force enable
+ufw reload
